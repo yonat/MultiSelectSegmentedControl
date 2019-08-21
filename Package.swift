59a9b66830632f8e4d7577f0910ffcbca116203f
Yonat Sharon <yonat@ootips.org>
2019-08-21 23:20:18 +0300

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "MultiSelectSegmentedControl",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library( name: "MultiSelectSegmentedControl", targets: ["MultiSelectSegmentedControl"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/yonat/SweeterSwift", from: "1.0.2")
+    ],
+    targets: [
+        .target(name: "MultiSelectSegmentedControl", dependencies: ["SweeterSwift"], path: "Sources")
+    ],
+    swiftLanguageVersions: [.v5]
+)
